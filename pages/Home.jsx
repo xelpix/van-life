1bbbc041233175dc5d25e141c2c6fa9676414ecd
xelpix <artfomen@gmail.com>
2023-05-07 15:54:12 +0600

    Added host routes

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react"
+import { Link } from "react-router-dom"
 
-// look! our button are a Link here! ( same as in about!)
 export default function Home() {
-  return (
-    <div className="home-container">
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make
-        your perfect road trip.
-      </p>
-      <Link to="vans">Find your van</Link>
-    </div>
-  );
-}
+    return (
+        <div className="home-container">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+        </div>
+    )
+};
